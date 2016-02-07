9074b01539b5a1de0bfb3c61b5bd80182a131ebe
Martin Galese <martin@galese.net>
2016-02-07 15:21:30 -0500

    8 way motion

--- src/DjikstraMap.cpp
+++ src/DjikstraMap.cpp
@@ -46,7 +46,13 @@ void DjikstraMap::nextStepFrom(int x, int y, int &n_x, int &n_y) {
   int down = get(x, y + 1);
   int left = get(x - 1, y);
   int right = get(x + 1, y);
-  std::vector<int> directions = {up, down, left, right};
+  int upleft = get(x - 1, y - 1);
+  int upright = get(x + 1, y - 1);
+  int downleft = get(x - 1, y + 1);
+  int downright = get(x + 1, y + 1);
+
+  std::vector<int> directions = {up,     down,    left,     right,
+                                 upleft, upright, downleft, downright};
 
   int minimum = min(directions);
   if (minimum == up)
@@ -57,8 +63,21 @@ void DjikstraMap::nextStepFrom(int x, int y, int &n_x, int &n_y) {
     n_x = x - 1;
   else if (minimum == right)
     n_x = x + 1;
+  else if (minimum == upright) {
+    n_x = x + 1;
+    n_y = y - 1;
+  } else if (minimum == upleft) {
+    n_x = x - 1;
+    n_y = y - 1;
+  } else if (minimum == downright) {
+    n_x = x + 1;
+    n_y = y + 1;
+  } else if (minimum == downleft) {
+    n_x = x - 1;
+    n_y = y + 1;
+  }
 
-  printf("[%i,%i]=>[%i, %i]\n", x, y, n_x, n_y);
+  // printf("[%i,%i]=>[%i, %i]\n", x, y, n_x, n_y);
 }
 
 bool DjikstraMap::canReachFrom(int x, int y) { return true; }
