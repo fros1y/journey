455275732dfc0f1bdd5ce2e090ee03ca71f73b66
Martin Galese <martin@galese.net>
2016-02-13 13:27:08 -0500

    clamp utility function

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,18 @@
 //
 // Created by Martin Galese on 2/8/16.
 //
+#include <algorithm>
 
 #ifndef JOURNEY_UTILS_H
 #define JOURNEY_UTILS_H
 
 int min(std::__1::vector<int> list);
+
+
+template <typename T>
+T clamp(T in, T low, T high)
+{
+  return std::min(std::max(in, low), high);
+}
 
 #endif //JOURNEY_UTILS_H
