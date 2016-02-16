cc97edc19c985f570aef19e92f203d8f5d31b36b
Martin Galese <martin@galese.net>
2016-02-16 12:54:52 -0500

    boost::graph layouts

--- src/MapGen.cpp
+++ src/MapGen.cpp
@@ -12,7 +12,21 @@
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/graph/random_layout.hpp>
 #include <boost/graph/make_connected.hpp>
+#include <boost/graph/boyer_myrvold_planar_test.hpp>
+#include <boost/graph/make_biconnected_planar.hpp>
 
+
+#include <boost/graph/fruchterman_reingold.hpp>
+#include <boost/graph/random_layout.hpp>
+#include <boost/graph/kamada_kawai_spring_layout.hpp>
+#include <boost/graph/circle_layout.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/point_traits.hpp>
+#include <boost/random/linear_congruential.hpp>
+#include <iostream>
+#include <boost/limits.hpp>
+#include <fstream>
+#include <string>
 
 bool MapGen::ellipseFill(int leftMost,
                       int rightMost,
@@ -86,33 +100,94 @@ void MapGen::floodFill(const int x, const int y, const Element fill) {
   if (x < width - 1 && y > 0) floodFill(x + 1, y - 1, fill);
 }
 
+enum vertex_position_t { vertex_position };
+namespace boost { BOOST_INSTALL_PROPERTY(vertex, position); }
+typedef boost::square_topology<>::point_type point;
+
+typedef boost::adjacency_list<boost::vecS,
+                              boost::vecS,
+                              boost::undirectedS,
+                              boost::property<boost::vertex_index_t, int,
+                                              boost::property<vertex_position_t, point> >,
+                              boost::property<boost::edge_weight_t, double> > Graph;
+
+typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
+typedef boost::graph_traits<Graph>::vertex_iterator vertex_iterator;
+
+
+struct kamada_kawai_done {
+  kamada_kawai_done() : last_delta() { }
+
+  template<typename Graph>
+  bool operator()(double delta_p,
+                  typename boost::graph_traits<Graph>::vertex_descriptor /*p*/,
+                  const Graph & /*g*/,
+                  bool global) {
+    if (global) {
+      double diff = last_delta - delta_p;
+      if (diff < 0) diff = -diff;
+      last_delta = delta_p;
+      return diff < 0.01;
+    } else {
+      return delta_p < 0.01;
+    }
+  }
+
+  double last_delta;
+};
+
 void MapGen::init() {
-  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
-  typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
-  typedef boost::graph_traits<Graph>::vertex_iterator vertex_iterator;
-
-  Graph map_graph;
-
+  std::unique_ptr<Graph> map_graph;
   boost::random::mt19937 rng;
-  boost::generate_random_graph(map_graph, 30, 30, rng, true, true);
-  boost::make_connected(map_graph);
 
-  std::pair<edge_iterator, edge_iterator> ei = boost::edges(map_graph);
-  std::pair<vertex_iterator, vertex_iterator> vi = boost::vertices(map_graph);
+  do {
+    map_graph = std::make_unique<Graph>();
+    boost::generate_random_graph(*map_graph, 10, 10, rng, true, true);
+    boost::make_connected(*map_graph);
+  } while (!boost::boyer_myrvold_planarity_test(*map_graph));
+
+  //boost::circle_graph_layout(*map_graph, get(vertex_position, *map_graph), 5.0);
+  boost::random_graph_layout(*map_graph, get(vertex_position, *map_graph), boost::square_topology<>(1.0));
+
+  bool ok = kamada_kawai_spring_layout(*map_graph,
+                                       get(vertex_position, *map_graph),
+                                       get(boost::edge_weight, *map_graph),
+                                       boost::square_topology<>(1.0),
+                                       boost::side_length(1.0),
+                                       kamada_kawai_done());
+
+  auto positions = get(vertex_position, *map_graph);
+
+  std::pair<edge_iterator, edge_iterator> ei = boost::edges(*map_graph);
+  std::pair<vertex_iterator, vertex_iterator> vi = boost::vertices(*map_graph);
 
   std::map<int, std::shared_ptr<Room> > rooms;
+
+  double min_x = 1e10;
+  double max_x = 0;
+  double min_y = 1e10;
+  double max_y = 0;
+
+  for (vertex_iterator it = vi.first; it != vi.second; ++it) {
+    auto x = positions[*it][0];
+    auto y = positions[*it][1];
+    min_x = std::min(min_x, x);
+    min_y = std::min(min_y, y);
+    max_x = std::max(max_x, x);
+    max_y = std::max(max_y, y);
+  }
 
   bool playerRoom = true;
   for (vertex_iterator it = vi.first; it != vi.second; ++it) {
     int x, y;
 
-    if(playerRoom) {
-      x = world->currLevel->width/2;
-      y = world->currLevel->height/2;
+    if (playerRoom) {
+      x = world->currLevel->width / 2;
+      y = world->currLevel->height / 2;
       playerRoom = false;
     } else {
-      x = world->rnd->getInt(2, width - 2);
-      y = world->rnd->getInt(2, height - 2);
+      x = maprange(positions[*it][0], min_x, max_x, 1, width-2);
+      y = maprange(positions[*it][1], min_y, max_y, 1, height-2);
     }
 
     auto w = world->rnd->getInt(3, 20);
@@ -123,7 +198,7 @@ void MapGen::init() {
   }
 
   for (edge_iterator it = ei.first; it != ei.second; ++it) {
-    buildCorridor(*rooms[boost::source(*it, map_graph)], *rooms[boost::target(*it, map_graph)]);
+    buildCorridor(*rooms[boost::source(*it, *map_graph)], *rooms[boost::target(*it, *map_graph)]);
   }
 }
 
@@ -158,14 +233,19 @@ bool MapGen::buildCorridor(const Room &r1, const Room &r2) {
   };
 
   auto repeatSteps = [&](const int size) {
-    do {} while (step(x, r2.center.x, size) || step(y, r2.center.y, size));
+    bool xtest, ytest;
+    do {
+      xtest = step(x, r2.center.x, size);
+      ytest= step(y, r2.center.y, size);
+    } while (xtest || ytest);
   };
 
-  repeatSteps(13);
+
   repeatSteps(7);
   repeatSteps(5);
-  repeatSteps(3);
   repeatSteps(2);
+  repeatSteps(13);
+  repeatSteps(3);
   repeatSteps(1);
 
   return true;
